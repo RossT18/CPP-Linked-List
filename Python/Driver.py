a8d8738cf24f9876045b3afb04ec9e627f082e5e
Ross Tinsley <rtinsley1199@gmail.com>
2021-03-21 23:15:31 +0000

    Add Python base classes

---
+++ Python/Driver.py
@@ -0,0 +1,7 @@
+from LinkedList import *
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ Python/LinkedList.py
@@ -0,0 +1,7 @@
+class Node:
+    def __init__():
+        pass
+
+class LinkedList:
+    def __init__(self):
+        pass
